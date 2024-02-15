abd6339e927058300e7a5da14fcc44838ce2fde3
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-15 17:09:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+	let totalCost = 0;
+	arr.sort((a,b)=>a-b);
+	for(let i=1;i<arr.length;i++){
+		let combinedLength = arr[i-1]+arr[i];
+		totalCost += combinedLength;
+		arr[i] = combinedLength;
+		arr.sort((a,b)=>a-b);
+	}
+	return totalCost;
   
 }
 
